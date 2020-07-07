6532051c3fc2bec122525ef12a6dc5df1d8f42fb
Justin McCaleb <justinleemccaleb@gmail.com>
2020-07-07 12:08:59 -0500

    completed Basic Statments part 2 exercise

---
+++ select_exercises.sql
@@ -0,0 +1,19 @@
+USE codeup_test_db;
+
+SELECT 'Pink Floyd' AS 'All Albums From';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'Sgt. Pepper''s Lonely Hearts Club Band' AS 'Album Release Year';
+SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+SELECT 'Nevermind' AS 'Genre For';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT 'The 90''s' AS 'Album Decade';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT 'Less than 20 Million Sales' AS 'Albums';
+SELECT name FROM albums WHERE sales < 20.0;
+
+SELECT 'Rock' AS 'Genre';
+SELECT name FROM albums WHERE genre = 'Rock';
